c1e4f100847d1a17da37d1d2b1333b61299c34d5
azam2802 <azamumarov182@gmail.com>
2024-08-08 22:29:35 +0600

    add: caution in Home.tsx component

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Hero from "./Sections/Hero/Hero";
 const Home: React.FC = () => {
   return (
     <main>
+      <h1 style={{textAlign: "center", color: "#E32636", fontSize:"50px"}}>!!! THIS WEBSITE IS ON DEVELOPMENT STAGE !!!</h1>
       <Hero />
     </main>
   );
